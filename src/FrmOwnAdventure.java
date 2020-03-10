1ff3cca0c276e5f344ec90a2362cd674eaf5edfd
ergra0573 <ergra0573@7L1M6Y1.Student.UGDSB.ED>
2020-03-10 15:21:10 -0400

    update

--- src/FrmOwnAdventure.java
+++ src/FrmOwnAdventure.java
@@ -35,7 +35,6 @@ public class FrmOwnAdventure extends javax.swing.JFrame {
         txtUserInput = new javax.swing.JTextField();
         txtDesicion1 = new javax.swing.JTextArea();
         txtDesicion2 = new javax.swing.JTextArea();
-        btnStart = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -64,13 +63,6 @@ public class FrmOwnAdventure extends javax.swing.JFrame {
         txtDesicion2.setRows(5);
         txtDesicion2.setText("2)\nGo swimming");
 
-        btnStart.setText("Start again");
-        btnStart.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnStartActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -84,8 +76,7 @@ public class FrmOwnAdventure extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createSequentialGroup()
                             .addGap(0, 0, Short.MAX_VALUE)
                             .addComponent(txtUserInput, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(51, 51, 51)
-                            .addComponent(btnStart))
+                            .addGap(136, 136, 136))
                         .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
                             .addGap(22, 22, 22)
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -117,10 +108,8 @@ public class FrmOwnAdventure extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtDesicion2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtDesicion1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txtUserInput, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnStart, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addComponent(txtUserInput, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -140,6 +129,7 @@ public class FrmOwnAdventure extends javax.swing.JFrame {
 
     private void txtUserInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUserInputActionPerformed
         int answer = Integer.parseInt(txtUserInput.getText());
+        String answerYN = txtUserInput.getText();
         if (answer == 1)
         {
         lblStory1.setText("You find enough food for a few weeks");
@@ -167,25 +157,24 @@ public class FrmOwnAdventure extends javax.swing.JFrame {
             lblStory1.setText("");
             lblStory2.setText("Invalid input.");
             lblStory3.setText("Input must be one of the options");
-            lblStory4.setText("Go back to the start.");
+            lblStory4.setText("Would you like to go back to the start?");
             txtDesicion1.setText(" ");
             txtDesicion2.setText(" ");
+            if ((answerYN.equals ("Yes"))||(answerYN.equals("yes")))
+            {
+                lblStory1.setText("Instructions: write the number of your desicion.");
+                lblStory2.setText("You are a plane crash survivor trying to stay alive in an island.");
+                lblStory3.setText("You have to try to survive until you get rescued.");
+                lblStory4.setText("The first thing you do is...");
+                txtDesicion1.setText("1)\n" +
+                            "look around the\n" +
+                            "plane for food and\n" +
+                            "tools");
+                txtDesicion2.setText("2)\n" +
+                            "Go swimming");
+            }
         }
-        
     }//GEN-LAST:event_txtUserInputActionPerformed
-
-    private void btnStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStartActionPerformed
-        lblStory1.setText("Instructions: write the number of your desicion.");
-        lblStory2.setText("You are a plane crash survivor trying to stay alive in an island.");
-        lblStory3.setText("You have to try to survive until you get rescued.");
-        lblStory4.setText("The first thing you do is...");
-        txtDesicion1.setText("1)\n" +
-                    "look around the\n" +
-                    "plane for food and\n" +
-                    "tools");
-        txtDesicion2.setText("2)\n" +
-                    "Go swimming");
-    }//GEN-LAST:event_btnStartActionPerformed
 
     /**
      * @param args the command line arguments
@@ -223,7 +212,6 @@ public class FrmOwnAdventure extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnStart;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblStory1;
     private javax.swing.JLabel lblStory2;
